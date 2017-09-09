bc673dcd508c2e8faead40823d2b517453408b4b
AliceCodeZhang <31360564+AliceCodeZhang@users.noreply.github.com>
2017-09-09 08:59:36 +0800

    test

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual('foo'.upper(), 'FOO1')
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
